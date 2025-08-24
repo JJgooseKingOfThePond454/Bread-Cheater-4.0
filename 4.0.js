9c93cc79cc3ac08f0a8e761644175a84fb6927b9
JJgooseKingOfThePond454 <161132773+JJgooseKingOfThePond454@users.noreply.github.com>
2025-08-23 22:19:56 -0400

    Add files via upload

---
+++ 4.0.js
@@ -0,0 +1,9 @@
+
+const TbuttonElList = document.querySelectorAll('.Tbutton');
+
+TbuttonElList.forEach(TbuttonEL => {
+    TbuttonEL.addEventListener('click', () => {
+        TbuttonEL.classList.add('special');
+    });
+});
+
